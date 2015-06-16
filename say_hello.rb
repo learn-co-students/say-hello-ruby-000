c369f3e743ec2bb4cd419c76acbaf6a93a47b7c4
edviq <elukyanov@gmail.com>
2015-06-15 22:30:08 -0400

    Method

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,9 @@
-# Build your say_hello method here
+def say_hello(name="Ruby Programmer")
+
+puts "Hello #{name}!"
+
+end
+
+# i want to see my name with the word hello and if it's not my name 
+# i should see "hello ruby programmer!"
+# method - perform an action
